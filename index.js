138a12e8b638b54bdae20450d171b4ec96d44853
Matthew Robinson <matt_robinson1@yahoo.com>
2019-12-16 13:24:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function fetchBooks() {
   return fetch ('https://anapioficeandfire.com/api/books')
     .then(function(response) {
-      return response.json 
+      return response.json() 
+      
       
       
     
